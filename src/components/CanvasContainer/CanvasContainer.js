6701c8911bb52e9dfda3f956f2f1429a375ae103
alex-chemus <aachemus@gmail.com>
2021-12-25 11:37:09 +0500

    removed extra rerendering
    now previous and next versions are compared and only new objects are rendered

--- src/components/CanvasContainer/CanvasContainer.js
+++ src/components/CanvasContainer/CanvasContainer.js
@@ -24,6 +24,8 @@ const CanvasContainer = () => {
   // store
   const canvasObjects = useSelector(state => state.canvasObjects)
   const currentTool = useSelector(state => state.currentTool)
+  const canvasVersions = useSelector(state => state.canvasVersions)
+  const interimVersions = useSelector(state => state.interimVersions)
   const dispatch = useDispatch()
 
 
@@ -50,7 +52,7 @@ const CanvasContainer = () => {
     }
     // remove all children, and then append them,
     // so removed canvas objects aren't rendered
-    const children = containerRef.current.children
+    /*const children = containerRef.current.children
     for (let i=0; i<children.length; i++) {
       children[i].remove()
     }
@@ -60,7 +62,23 @@ const CanvasContainer = () => {
         width: containerWidth,
         height: containerHeight
       })
+    })*/
+    const nextFull = [...canvasObjects]
+    const versions = [...canvasVersions].concat([...interimVersions])
+    const prevFull = [...versions[versions.length-2]]
+
+    const [prev, next] = removeDuplicates(prevFull, nextFull)
+    next.forEach(item => {
+      if (!containerRef.current.contains(item.canvas)) {
+        containerRef.current.append(item.canvas)
+      }
+      render(item, {
+        width: containerWidth,
+        height: containerHeight
+      })
     })
+    prev.forEach(item => item.canvas.remove())
+
   }, [canvasObjects])
 
   // render currentObject on change
@@ -90,7 +108,50 @@ const CanvasContainer = () => {
   }, [currentTool])
 
 
-  // methods
+  // utility methods
+  function treesEqual(tree1, tree2) {
+    // проверка на соответсвие ключей деревьев друг другу
+    for (let key in tree1) {
+      if (!Object.keys(tree2).includes(key)) return false
+    }
+    for (let key in tree2) {
+      if (!Object.keys(tree1).includes(key)) return false
+    }
+  
+    // проверка на соответствие значений деревьев
+    for (let i=0; i<Object.values(tree1).length; i++) {
+      const arr1 = Object.values(tree1) // значения первого дерева
+      const arr2 = Object.values(tree2) // значения второго дерева
+
+      if (typeof arr1[i] !== 'object') { 
+        // не объект, проверка по примитивам
+        if (arr1[i] !== arr2[i]) return false
+      } else if (arr1[i] instanceof HTMLElement && arr2[i] instanceof HTMLElement) {
+        // html-объект, пропускаем
+      } else if (typeof arr1[i] === typeof arr2[i] && arr1[i] && arr2[i]) {
+        // два объекта, рекурсия
+        if ( !treesEqual(arr1[i], arr2[i]) ) return false
+      } // другой случай - оба значения null, проходит проверку всегда
+    }
+  
+    return true
+  }
+
+  function removeDuplicates(prev, next) {
+    next.forEach((nextItem, i) => {
+      prev.forEach((prevItem, j) => {
+        if ( treesEqual(nextItem, prevItem) ) {
+          prev.splice(j, 1)
+          setTimeout(() => {
+            next.splice(i, 1) 
+          });
+        }
+      })
+    })
+
+    return [prev, next]
+  }
+
   function createCanvas(containerRef) {
     if (!containerRef?.current) return
     const canvas = document.createElement('canvas')
@@ -100,6 +161,8 @@ const CanvasContainer = () => {
     return canvas
   }
 
+
+  // methods
   function draw() {
     if (!endPosition || !startPosition) return
     switch (currentTool) {
