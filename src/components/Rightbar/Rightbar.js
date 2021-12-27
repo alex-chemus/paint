c3ef61b4b4f070696743c8cee6f0dae74eb60e71
alex-chemus <aachemus@gmail.com>
2021-12-27 17:03:03 +0500

    added layers drag'n'drop

--- src/components/Rightbar/Rightbar.js
+++ src/components/Rightbar/Rightbar.js
@@ -8,6 +8,10 @@ const Rightbar = () => {
   const [objects, setObjects] = useState([])
   const [containerWidth, setContainerWidth] = useState(document.body.clientWidth - 266 - 85)
   const [containerHeight, setContainerHeight] = useState(document.body.clientHeight - 100)
+  const [startY, setStartY] = useState()
+  const [startTop, setStartTop] = useState()
+  const [endY, setEndY] = useState()
+  const [clicked, setClicked] = useState()
 
 
   // store
@@ -47,6 +51,30 @@ const Rightbar = () => {
     }
   }
 
+  function swapElements(nodeA, nodeB) {
+    const parentA = nodeA.parentNode;
+    const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
+    // Move `nodeA` to before the `nodeB`
+    nodeB.parentNode.insertBefore(nodeA, nodeB);
+    // Move `nodeB` to before the sibling of `nodeA`
+    parentA.insertBefore(nodeB, siblingA);
+  }
+
+  function swapObjects(prevIndex, nextIndex) {
+    const prev = objects[prevIndex]
+    const next = objects[nextIndex]
+    objects.slice(prevIndex, 2, next, prev)
+  }
+
+  function swapZ(prevI, nextI) {
+    const list = objects
+    const aboba = list[prevI].z
+    list[prevI].z = list[nextI].z
+    list[nextI].z = aboba
+    list.sort((a, b) => a.z > b.z ? 1 : -1)
+    setObjects(list)
+  }
+
 
   // methods
   function select(id) {
@@ -78,30 +106,99 @@ const Rightbar = () => {
     return canvas
   }
 
+  function dragLayer(object, i, event) {
+    const li = event.currentTarget
+    li.classList.add(classes.draged)
+    const offset = (objects.length-1-i)*50
+    setStartY(event.clientY - offset - 50 - 79)
+    setStartTop(offset)
+    setClicked(true)
+  }
 
-  // jsx-objects
-  const layers = (
-    <ul>  
-      {
-        objects.map(item => {
-          const cls = [classes.layer]
-          if (item.id === currentLayer) cls.push(classes.selected)
-          const [width, height] = calcMiniCanvas()
-          console.log('item from layer object: ', item)
-          return (
-            <li 
-              className={cls.join(' ')}
-              key={item.z}
-              onClick={() => select(item.id)}
-            >
-              { drawCanvas(width, height, item) }
-              { capitalizeFirstLetter(item.type) }
-            </li>
-          )
-        })
-      } 
-    </ul>
-  )
+  function moveLayer(object, i, event) {
+    if (!clicked) return
+    const y = event.clientY
+    const li = event.currentTarget
+    const prev = li.previousElementSibling
+    const next = li.nextElementSibling
+    // y coordinate from original location
+    const shift = y - 50 - 79 - startTop - startY
+    
+    // element is dragged down
+    if (shift > li.clientHeight && prev) {
+      swapZ(i, i-1)
+      const offset = (objects.length-i)*50
+      setObjects(objects.map((item, j) => {
+        if (i-1 !== j) return item 
+        return {
+          ...item,
+          style: {
+            top: '0px'
+          }
+        }
+      }))
+      setStartTop(offset)
+    } else if (-shift > li.clientHeight && next) {
+      swapZ(i, i+1)
+      const offset = (objects.length-i)*50
+      setObjects(objects.map((item, j) => {
+        if (i+1 !== j) return item 
+        return {
+          ...item,
+          style: {
+            top: '0px'
+          }
+        }
+      }))
+      setStartTop(offset)
+    } else {
+      setObjects(objects.map((item, j) => {
+        if (i !== j) return item
+        return {
+          ...item,
+          style: {
+            top: `${shift}px`
+          }
+        }
+      }))
+    }
+    setEndY(y)
+  }
+
+  function dropLayer(object, i, event) {
+    event.currentTarget.classList.remove(classes.draged)
+    console.log('drop layer')
+    setClicked(false)
+    setObjects(objects.map((item, j) => {
+      if (i !== j) return item
+      return {
+        ...item,
+        style: { top: null }
+      }
+    }))
+  }
+
+  function renderItem(object, i) {
+    const [width, height] = calcMiniCanvas()
+    const cls = [classes.layer]
+    object?.class && cls.push(object.class)
+    if (object.id === currentLayer) cls.push(classes.selected)
+    return (
+      <li
+        className={cls.join(' ')}
+        key={object.z}
+        onClick={() => select(object.id)}
+        onMouseDown={e => dragLayer(object, i, e)}
+        onMouseUp={e => dropLayer(object, i, e)}
+        onMouseLeave={e => dropLayer(object, i, e)}
+        onMouseMove={e => moveLayer(object, i, e)}
+        style={object?.style}
+      >
+        { drawCanvas(width, height, object) }
+        { capitalizeFirstLetter(object.type) }
+      </li>
+    )
+  }
 
 
   return (
@@ -110,7 +207,9 @@ const Rightbar = () => {
     >
       <section className={classes.layers}>
         <h2>Layers</h2>
-        { layers }
+        <ul> {
+          objects.map(renderItem)
+        } </ul>
       </section>
 
       <div className={classes.line}></div>
